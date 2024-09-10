803aaa3fbfc6133311f9ab0455f6400424e9660a
JCY <chung4171@naver.com>
2024-09-10 19:15:50 +0900

    develop

--- src/main/java/com/wnergy/smartMr/controller/viewController.java
+++ src/main/java/com/wnergy/smartMr/controller/viewController.java
@@ -1,8 +1,17 @@
 package com.wnergy.smartMr.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class viewController {
+
+    @GetMapping("/")
+    public String home() {
+        return "homePage";
+    }
+
 
 
 }
