02bfd45e5b5426347b37b77475c94baa69b3d6f2
정찬영 <94053008+chan0e@users.noreply.github.com>
2024-09-10 17:19:37 +0900

    Add files via upload

---
+++ smartMr/src/main/java/com/wnergy/smartMr/SmartMrApplication.java
@@ -0,0 +1,13 @@
+package com.wnergy.smartMr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMrApplication.class, args);
+	}
+
+}
